011508a090a278a921943fb783dfa9794c618aa6
Andre Meij <andre@itsmeij.com>
2019-08-06 11:18:49 +0200

    Blerg

--- app/models/exceptio/exception.rb
+++ app/models/exceptio/exception.rb
@@ -4,6 +4,12 @@ module Exceptio
   class Exception < ApplicationRecord
     has_many :instances, dependent: :destroy
 
+    delegate :message, to: :last_instance
+
+    def last_instance
+      instances.order(created_at: :desc).first
+    end
+
     class << self
       def for(exception)
         where(exception_class: exception.class.name, code_location: exception.backtrace.first).first_or_initialize
