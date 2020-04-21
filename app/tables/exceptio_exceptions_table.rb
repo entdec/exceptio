31021881195caad5a5492e6921d9f16fb5f319e6
Andre Meij <andre@itsmeij.com>
2020-04-21 11:52:04 +0200

    sorting and errors

--- app/tables/exceptio_exceptions_table.rb
+++ app/tables/exceptio_exceptions_table.rb
@@ -7,10 +7,10 @@ class ExceptioExceptionsTable < ActionTable::ActionTable
   column(:message) { |exception| exception.message.to_s.truncate(40) }
   column(:code_location) { |exception| exception.code_location.to_s.gsub(Rails.root.to_s, '').truncate(20) }
   column(:count) { |exception| exception.instances.size }
-  column(:last_occurence) { |exception| ln exception.updated_at }
+  column(:last_occurence, sort_field: :updated_at) { |exception| ln exception.updated_at }
   column(:actions) { |exception| link_to '<i class="fas fa-trash"></i>'.html_safe, exceptio.exception_path(exception), method: :delete }
 
-  initial_order :mkey, :asc
+  initial_order :updated_at, :desc
 
   row_link { |exception| exceptio.exception_path(exception) }
 
