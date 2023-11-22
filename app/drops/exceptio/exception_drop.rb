0edf3bae741969869883c0c612557992c279fd41
Tom de Grunt <tom@degrunt.nl>
2023-11-22 14:53:17 +0100

    Add code_location

--- app/drops/exceptio/exception_drop.rb
+++ app/drops/exceptio/exception_drop.rb
@@ -1,5 +1,9 @@
 module Exceptio
   class ExceptionDrop < ApplicationDrop
-    delegate :id, :exception_class, :code_location, :instances, :instances_count, :last_instance_at , to: :@object
+    delegate :id, :exception_class, :backtrace, :instances, :instances_count, :last_instance_at , to: :@object
+
+    def code_location
+      backtrace.first
+    end
   end
 end
