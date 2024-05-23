b2ab34a013d65624c4689098c7e1c02414b0d1f1
Syed Waqas Ali Shah <s.waqas@rapidlogistics.nl>
2024-05-23 16:19:23 +0400

    fix updated_at ambiguity

--- app/tables/exceptio_exceptions_table.rb
+++ app/tables/exceptio_exceptions_table.rb
@@ -7,7 +7,7 @@ class ExceptioExceptionsTable < ActionTable::ActionTable
   column(:message) { |exception| exception.message.to_s.truncate(40) }
   column(:code_location) { |exception| exception.code_location.to_s.gsub(Rails.root.to_s, '').truncate(20) }
   column(:count)
-  column(:last_occurence, sort_field: :updated_at) { |exception| ln exception.updated_at }
+  column(:last_occurence, sort_field: "exceptio_exceptions.updated_at") { |exception| ln exception.updated_at }
   column(:actions) do |exception|
     link_to '<i class="fal fa-trash"></i>'.html_safe, exceptio.exception_path(exception),
             data: { turbo_method: :delete }
