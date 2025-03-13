4eec62cd23ed0089c979f607c6a9b0dceacd05c0
shubh <S.Seetharamasetty@rapidlogistics.nl>
2025-03-13 14:10:34 +0400

    ExceptioExceptionsTable sort

--- app/tables/exceptio_exceptions_table.rb
+++ app/tables/exceptio_exceptions_table.rb
@@ -4,11 +4,11 @@ class ExceptioExceptionsTable < ActionTable::ActionTable
   model Exceptio::Exception
 
   column(:exception_class)
-  column(:message) { |exception| exception.message.to_s.truncate(40) }
+  column(:message, sortable: false) { |exception| exception.message.to_s.truncate(40) }
   column(:code_location) { |exception| exception.code_location.to_s.gsub(Rails.root.to_s, '').truncate(20) }
-  column(:count) { |exception| exception.instances.size }
+  column(:count, sort_field: :instances_count ) { |exception| exception.instances_count }
   column(:last_occurence, sort_field: :updated_at) { |exception| ln exception.updated_at }
-  column(:actions) { |exception| link_to '<i class="fal fa-trash"></i>'.html_safe, exceptio.exception_path(exception), data: { turbo_method: :delete }}
+  column(:actions, sortable: false) { |exception| link_to '<i class="fal fa-trash"></i>'.html_safe, exceptio.exception_path(exception), data: { turbo_method: :delete }}
 
   initial_order :last_occurence, :desc
 
